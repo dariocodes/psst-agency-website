679b7e0b8e69035f18b475fe93044778b1868b91
Dario <dariomadau@hotmail.com>
2023-04-28 13:33:35 +0200

    added nav

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ const observer = new IntersectionObserver(
 );
 
 hiddenElements.forEach((section) => observer.observe(section));
+
+const hamburger = document.querySelector(".menu");
+const stripe1 = document.querySelector(".stripe1");
+const stripe2 = document.querySelector(".stripe2");
+
+hamburger.addEventListener("click", () => {
+  stripe1.classList.toggle("open");
+  stripe2.classList.toggle("open2");
+});
