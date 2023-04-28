1ac7411a8e9ee18535e7851a53f23f678b876c10
Dario <dariomadau@hotmail.com>
2023-04-28 14:01:35 +0200

    mobile menu done

--- app.js
+++ app.js
@@ -18,8 +18,14 @@ hiddenElements.forEach((section) => observer.observe(section));
 const hamburger = document.querySelector(".menu");
 const stripe1 = document.querySelector(".stripe1");
 const stripe2 = document.querySelector(".stripe2");
+const ul = document.querySelector("ul");
+const li = document.querySelectorAll("li");
 
 hamburger.addEventListener("click", () => {
   stripe1.classList.toggle("open");
   stripe2.classList.toggle("open2");
+  ul.classList.toggle("openmobile");
+  li.forEach((li) => li.classList.toggle("mobile"));
 });
+
+console.log(ul);
