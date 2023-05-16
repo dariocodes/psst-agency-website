d0e3f894fd8f92347c22d48f2e62a4c4fbf7588e
Dario <dariomadau@hotmail.com>
2023-05-16 15:27:20 +0200

    big changes

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll('.hidden');
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      entry.target.classList.toggle("show", entry.isIntersecting);
+      entry.target.classList.toggle('show', entry.isIntersecting);
     });
   },
   {
-    rootMargin: "-100px 0px -210px 0px",
+    rootMargin: '-100px 0px -210px 0px',
     threshold: 0,
   }
 );
 
 hiddenElements.forEach((section) => observer.observe(section));
 
-const hamburger = document.querySelector(".menu");
-const stripe1 = document.querySelector(".stripe1");
-const stripe2 = document.querySelector(".stripe2");
-const ul = document.querySelector("ul");
-const li = document.querySelectorAll("li");
+const hamburger = document.querySelector('.menu');
+const stripe1 = document.querySelector('.stripe1');
+const stripe2 = document.querySelector('.stripe2');
+const ul = document.getElementById('mobile-ul');
+const li = document.querySelectorAll('.mobile-li');
 
-hamburger.addEventListener("click", () => {
-  stripe1.classList.toggle("open");
-  stripe2.classList.toggle("open2");
-  ul.classList.toggle("openmobile");
+console.log(li);
+
+hamburger.addEventListener('click', () => {
+  stripe1.classList.toggle('open');
+  stripe2.classList.toggle('open2');
+  ul.classList.toggle('openmobile');
   li.forEach((li) => {
-    li.classList.toggle("mobile");
+    li.classList.toggle('mobile');
   });
 });
 
 li.forEach((item) =>
-  item.addEventListener("click", () => {
-    stripe1.classList.toggle("open");
-    stripe2.classList.toggle("open2");
-    ul.classList.toggle("openmobile");
+  item.addEventListener('click', () => {
+    stripe1.classList.toggle('open');
+    stripe2.classList.toggle('open2');
+    ul.classList.toggle('openmobile');
     li.forEach((li) => {
-      li.classList.toggle("mobile");
+      li.classList.toggle('mobile');
     });
   })
 );
